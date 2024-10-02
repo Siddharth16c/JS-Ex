c81e7f55e139022419338c1354b48855e847c5e2
Siddharth Jeswani <61928506+Siddharth16c@users.noreply.github.com>
2024-10-02 20:10:39 +0000

    NA
    NA

---
+++ 01_BASICS/DataTypes.js
@@ -0,0 +1,16 @@
+let variable
+
+console.log(variable);   ///undefined
+
+
+var num = 23
+let name = 'Jeswani' + "Siddharth" 
+
+let relationship = null
+console.log(typeof(relationship));
+
+
+console.log(typeof num);
+
+
+console.log(typeof(name));
---
+++ 01_BASICS/Variables.js
@@ -0,0 +1,19 @@
+
+
+
+function print()
+{
+
+    console.log("num is "+num)
+    var num = 20
+    let num2 = 30
+
+    console.log("num is "+num+" num2 is "+num2)
+    
+}
+
+print()
+
+
+
+
