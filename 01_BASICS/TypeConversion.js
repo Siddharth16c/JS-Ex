f9a164942f9fc756063858f65c653c9d66ce64ed
Siddharth Jeswani <61928506+Siddharth16c@users.noreply.github.com>
2024-10-02 20:28:40 +0000

    type conversions

---
+++ 01_BASICS/TypeConversion.js
@@ -0,0 +1,32 @@
+let num = "44abc"
+
+
+let newNum = Number(num)
+console.log(newNum);
+
+console.log(typeof newNum)
+
+let num2 = null
+
+let newNum2 = Number(num2)
+console.log(newNum2);
+console.log(typeof newNum2);
+
+let number = 0x2323
+let number2 = 0o3445
+
+console.log( number);
+console.log( number2);
+
+let boolNumber = Boolean(number)
+
+console.log(boolNumber);
+console.log(typeof boolNumber);
+
+
+
+
+
+
+
+
